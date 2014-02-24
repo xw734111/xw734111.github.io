ea564ba22a839e589e9fd44566204dd36f71ff17
Xiaoxu Wang <xw734111@ohio.edu>
2014-02-24 15:30:00 -0500

    files added.

---
+++ public/core.js
@@ -0,0 +1,61 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	// when landing on the page, get all todos and show them
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+	
+	// when 
+	$scope.confirmTodo = function() {
+		var r=confirm("Are you sure to add: " + $scope.formData.text);
+			if (r==true)
+  			{
+  				x="You pressed OK!";
+  			}
+			else
+  			{
+  				x="You pressed Cancel!";
+  			}
+			$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$('input').val('');
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+		
+	};
+	
+	// when submitting the add form, send the text to the node API
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$('input').val('');
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	// delete a todo after checking it
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos/' + id)
+			.success(function(data) {
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+}
