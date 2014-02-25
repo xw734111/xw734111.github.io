baa937c9c53ecfa03b1db66f44c59572312c4e41
Xiaoxu Wang <xw734111@ohio.edu>
2014-02-24 19:32:57 -0500

    Heroku

---
+++ Heroku/ruby-1.9.2/bin/dotenv.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "C:/Users/LoanerPC1/Repo/xw734111.github.io/Heroku/ruby-1.9.2/bin/dotenv" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
